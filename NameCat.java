ffa9f74dfba59381c626c79f22f7e25cd2d058ec
greeem-ea <hsj7362@gmail.com>
2025-02-09 11:44:54 +0900

    Add files via upload

---
+++ NameCat.java
@@ -0,0 +1,13 @@
+package 복학1;
+
+public class NameCat {
+	
+	private String name = "예쁜이";
+	private int age = 3;
+	
+	public String toString() {
+		return "NameCat[name=" + name + ",age=" +age+ "]";
+		
+	}
+
+}
---
+++ NameCatTest.java
@@ -0,0 +1,15 @@
+package 복학1;
+
+public class NameCatTest {
+	public static void main(String args[]) {
+	
+	NameCat c;
+	c = new NameCat();
+	
+	System.out.println(c.toString());
+	System.out.println(c);
+	
+
+
+	}
+}
